d797c57da4d3c6ced41cb61a931b79eb38ec92a3
Roger Jose Ulate Rivera <roger.p.designe357@gmail.com>
2020-06-25 02:10:26 -0600

    Todo arriba

--- MVC-ATM-Training/App_Start/RouteConfig.cs
+++ MVC-ATM-Training/App_Start/RouteConfig.cs
@@ -12,12 +12,30 @@ namespace MVC_ATM_Training
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            // Nueva ruta agregada por mi.
+            // EJEMPLO DE GET REQUEST
+            // Parameters will not be part of URL
+            /*
+            routes.MapRoute(
+                name: "Serial number",
+                url: "serial/{letterCase}",
+                defaults: new { controller = "Home", action = "Serial", letterCase = "upper"}
+            );
+            */
+
+            routes.MapRoute(
+                name: "Serial number",
+                url: "serial/{letterCase}",
+                defaults: new { controller = "Home", action = "Serial", letterCase = "upper" }
+            );
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
--- MVC-ATM-Training/Controllers/HomeController.cs
+++ MVC-ATM-Training/Controllers/HomeController.cs
@@ -8,11 +8,14 @@ namespace MVC_ATM_Training.Controllers
 {
     public class HomeController : Controller
     {
+        // GET /home/index
         public ActionResult Index()
         {
-            return View();
+            //return View();
+            return PartialView();
         }
 
+        // GET /home/about
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -25,6 +28,44 @@ namespace MVC_ATM_Training.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Foo()
+        {
+            return View("About");
+        }
+
+        // Ejemplo de GET REQUEST
+        /*
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5ATM1";
+
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+
+            return Content(serial);
+
+        }
+        */
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5ATM1";
+
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+
+            // return new HttpStatusCodeResult(403);
+
+
+            // Si quiero probar, por razones de seguridad debo poner un JSON behavior
+            return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
